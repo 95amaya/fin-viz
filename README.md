caccfc463665bee996fd6725cb90f586b66df5a3
Michael Amaya <michael.amaya09@gmail.com>
2023-11-05 02:43:33 +0000

    init working app

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fin-viz
-A financial visualization app
+A Financial Data Visualization app built with [streamlit](https://streamlit.io/)
+
+# Local Development Setup
+1. Install [Docker](https://www.docker.com/)
+2. Install [Visual Studio Code](https://code.visualstudio.com/)
+3. Install Visual Studio Code's [Remote Containers Extension](https://code.visualstudio.com/docs/devcontainers/tutorial)
+4. Start Docker
+5. Open codebase in remote container
+6. Run `Streamlit run streamlit-data-analysis.py` to run data visualization dashboard
+
+# Additonal Resources
+Check out the jupyter notebook used to experiment with python data slicing and ploty visualizations
---
+++ streamlit-data-analysis.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from enum import Enum
+from datetime import datetime
+from decouple import config
+import calendar
+
+DATA_FILE = config('DATA_FILE')
+
+# print(DATA_FILE)
+
+
+class Col(Enum):
+    TransactionDate = "Transaction Date"
+    Amount = "Amount"
+    TransactionType = "Transaction Type"
+    AccountType = "Account"
+    Description = "Description"
+    CustomTags = "Custom Tags"
+
+
+def toList(val: str):
+    return val.split(",")
+
+
+# @st.cache_data
+def get_data_from_csv():
+    df = pd.read_csv(DATA_FILE)
+    df[Col.TransactionDate.value] = pd.to_datetime(
+        df[Col.TransactionDate.value])
+    df[Col.CustomTags.value] = df.apply(lambda row: toList(str(
+        row[Col.CustomTags.value])), axis=1)
+    return df
+
+# helpful commands
+# print(df.head())
+# print(df.info())
+
+
+# ----- Main Application ---------
+st.set_page_config(page_title="Finance Dashboard",
+                   layout="wide")
+
+df = get_data_from_csv()
+
+# ----- SIDEBAR -----
+st.sidebar.header("Please Filter Here")
+account = st.sidebar.multiselect(
+    "Select Account:",
+    options=df[Col.AccountType.value].unique(),
+    default=df[Col.AccountType.value].unique()
+)
+
+transaction_type = st.sidebar.multiselect(
+    "Select Transaction Type:",
+    options=df[Col.TransactionType.value].unique(),
+    default=df[Col.TransactionType.value].unique()
+)
+
+custom_tags = st.sidebar.multiselect(
+    "Select Custom Tags:",
+    options=df[Col.CustomTags.value].explode().unique(),
+    default=df[Col.CustomTags.value].explode().unique()
+)
+
+qry = f'`{Col.AccountType.value}` == @account & `{Col.TransactionType.value}` == @transaction_type & `{Col.CustomTags.value}`.explode() in @custom_tags'
+# print(qry)
+df_selected = df.query(qry)
+
+grp_by_month = df.groupby([
+    Col.AccountType.value,
+    Col.TransactionType.value,
+    df[Col.TransactionDate.value].dt.month,
+])
+# pd.Grouper(key=Col.TransactionDate.value, freq='M'),
+# print(datetime.now())
+# print(grp_by_month.first())
+
+# print(df.info())
+# https://plotly.com/python/bar-charts/#grouped-bar-chart:~:text=avg%20of%20total_bill-,Bar%20Charts%20with%20Text,-New%20in%20v5.5
+
+# st.dataframe(grp_by_month.first(), use_container_width=True)
+st.dataframe(df_selected, use_container_width=True)
+
+
+def get_month_name(month_num: int):
+    return calendar.month_name[month_num]
+
+
+def get_df_grp(dfi: pd.DataFrame, filterByFunc, grpByFunc):
+    return dfi.loc[filterByFunc].groupby(grpByFunc)[Col.Amount.value]
+
+
+# print(df[Col.CustomTags.value].head())
+# print(df[Col.CustomTags.value].explode().head())
+# print(df[Col.CustomTags.value].explode() == 'NOISE')
+# print(df[Col.CustomTags.value].explode() != 'NOISE')
+
+# print(df[Col.TransactionDate.value] >= datetime(2023, 1, 1))
+
+viz_qry = (df[Col.CustomTags.value].explode() != 'NOISE')\
+    & (df[Col.TransactionDate.value] >= datetime(2023, 1, 1))\
+    & (df[Col.TransactionDate.value] < datetime(2023, 10, 1))\
+    & (df[Col.TransactionType.value] == 'DEBIT')
+vis_grp = df.loc[viz_qry]
+
+
+xVals = vis_grp[Col.TransactionDate.value].dt.month.unique()
+debit_per_month_group = pd.DataFrame(list(map(get_month_name, xVals)))
+debit_per_month_group[1] = vis_grp.loc[(df[Col.Amount.value] > 0)]\
+    .groupby(vis_grp[Col.TransactionDate.value].dt.month)[Col.Amount.value]\
+    .sum()\
+    .values
+
+debit_per_month_group[2] = vis_grp.loc[(df[Col.Amount.value] < 0)]\
+    .groupby(vis_grp[Col.TransactionDate.value].dt.month)[Col.Amount.value]\
+    .apply(lambda val: val.abs().sum())\
+    .values
+
+
+debit_per_month_group.columns = ["Month", "Income", "Spent"]
+debit_per_month_group
+
+# Ideal Plot
+# Month Name, Amount > 0, Amount < 0, Surplus / Deficit
+fig = px.bar(debit_per_month_group, x="Month",
+             y=debit_per_month_group.columns[1:], barmode='group')
+st.plotly_chart(fig, use_container_width=True)
